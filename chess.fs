fc8d3e8354e48fd9f8920f749155ccdf1d8870de
PPhorn <pernillehorn@outlook.com>
2019-01-04 11:38:49 +0100

    Added code snip to chess.fs and created report .tex file

--- chess.fs
+++ chess.fs
@@ -17,24 +17,24 @@ type chessPiece(color : Color) =
   /// A list of runs, which is a list of relative movements, e.g.,
   /// [[(1,0); (2,0);...]; [(-1,0); (-2,0)]...]. Runs must be
   /// ordered such that the first in a list is closest to the piece
-  /// at hand.      
+  /// at hand.
   abstract member candiateRelativeMoves : Position list list
   /// Available moves and neighbours ([(1,0); (2,0);...], [p1; p2])
   member this.availableMoves (board : Board) : (Position list * chessPiece list) =
-    board.getVacantNNeighbours this (*//§\label{chessPieceEnd}§*)
+    let possibleMoves, possibleKills = board.getVacantNNeighbours this (*//§\label{chessPieceEnd}§*)
 /// A board §\label{chessBoardBegin}§
 and Board () =
   let _array = Collections.Array2D.create<chessPiece option> 8 8 None
   /// Wrap a position as option type
   let validPositionWrap (pos : Position) : Position option =
     let (rank, file) = pos // square coordinate
-    if rank < 0 || rank > 7 || file < 0 || file > 7 
+    if rank < 0 || rank > 7 || file < 0 || file > 7
     then None
     else Some (rank, file)
   /// Convert relative coordinates to absolute and remove out
   /// of board coordinates.
   let relativeToAbsolute (pos : Position) (lst : Position list) : Position list =
-    let addPair (a : int, b : int) (c : int, d : int) : Position = 
+    let addPair (a : int, b : int) (c : int, d : int) : Position =
       (a+c,b+d)
     // Add origin and delta positions
     List.map (addPair pos) lst
@@ -43,16 +43,16 @@ and Board () =
   /// Board is indexed using .[,] notation
   member this.Item
     with get(a : int, b : int) = _array.[a, b]
-    and set(a : int, b : int) (p : chessPiece option) = 
+    and set(a : int, b : int) (p : chessPiece option) =
       if p.IsSome then p.Value.position <- Some (a,b)  (*//§\label{chessItemSet}§*)
       _array.[a, b] <- p
   /// Produce string of board for, e.g., the printfn function.
   override this.ToString() =
     let rec boardStr (i : int) (j : int) : string =
-      match (i,j) with 
+      match (i,j) with
         (8,0) -> ""
         | _ ->
-          let stripOption (p : chessPiece option) : string = 
+          let stripOption (p : chessPiece option) : string =
             match p with
               None -> ""
               | Some p -> p.ToString()
@@ -60,14 +60,14 @@ and Board () =
           let pieceStr = stripOption _array.[7-i,j]
           //let pieceStr = sprintf "(%d, %d)" i j
           let lineSep = " " + String.replicate (8*4-1) "-"
-          match (i,j) with 
-          (0,0) -> 
+          match (i,j) with
+          (0,0) ->
             let str = sprintf "%s\n| %1s " lineSep pieceStr
             str + boardStr 0 1
-          | (i,7) -> 
+          | (i,7) ->
             let str = sprintf "| %1s |\n%s\n" pieceStr lineSep
-            str + boardStr (i+1) 0 
-          | (i,j) -> 
+            str + boardStr (i+1) 0
+          | (i,j) ->
             let str = sprintf "| %1s " pieceStr
             str + boardStr i (j+1)
     boardStr 0 0
@@ -90,16 +90,16 @@ and Board () =
   /// find the list of all empty squares and list of neighbours
   member this.getVacantNNeighbours (piece : chessPiece) : (Position list * chessPiece list)  =
     match piece.position with
-      None -> 
+      None ->
         ([],[])
       | Some p ->
-        let convertNWrap = 
+        let convertNWrap =
           (relativeToAbsolute p) >> this.getVacantNOccupied
         let vacantPieceLists = List.map convertNWrap piece.candiateRelativeMoves
         // Extract and merge lists of vacant squares
         let vacant = List.collect fst vacantPieceLists
         // Extract and merge lists of first obstruction pieces and filter out own pieces
-        let opponent = 
+        let opponent =
           vacantPieceLists
-          |> List.choose snd 
+          |> List.choose snd
         (vacant, opponent)(*//§\label{chessBoardEnd}§*)
