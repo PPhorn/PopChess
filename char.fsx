cbbfb93b58741c61d0ed6c437019c8c2e32c108f
PPhorn <pernillehorn@outlook.com>
2019-01-13 14:03:39 +0100

    Cleaned folder

--- char.fsx
+++
@@ -1,3 +0,0 @@
-//BRUG TIL PRINT AF MULIGHEDER
-let input = "a4 a5"
-printfn "%A og %A" (int input.[0]) (int input.[3])
--- compileSimpleChess.sh
+++
@@ -1,4 +0,0 @@
-fsharpc -a chess.fs
-fsharpc -r chess.dll -a pieces.fs
-fsharpc -r chess.dll -r pieces.dll chessApp.fsx
-mono chessApp.exe
